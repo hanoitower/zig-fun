15bc6ef932903896ecee0a8bd3d9778a556fd385
Alexander Rummel <alexander.rummel@man.eu>
2021-04-05 20:03:00 +0200

    cube-chain: use cell_skip

--- cube-chain/src/main.zig
+++ cube-chain/src/main.zig
@@ -4,18 +4,20 @@ const stdout = std.io.getStdOut().writer();
 
 const cube_chain = [_]u8{ 2, 1, 1, 2, 1, 2, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 2 };
 
+const cell_skip = [_]i8{ 1, 5, 25, -25, -5, -1 };
+
 var cube_cells = [_]u8{99} ** (5 * 5 * 5);
 
 fn printCubeCells() !void {
-    var p: usize = 0;
-    var iy: usize = 0;
+    var p: i8 = 0;
+    var iy: u8 = 0;
     while (iy < 5) : (iy += 1) {
-        var ix: usize = 0;
+        var ix: u8 = 0;
         while (ix < 5) : (ix += 1) {
-            var iz: usize = 0;
+            var iz: u8 = 0;
             while (iz < 5) : (iz += 1) {
-                try stdout.print("{:3}", .{cube_cells[p]});
-                p += 1;
+                try stdout.print("{:3}", .{cube_cells[@intCast(usize, p)]});
+                p += cell_skip[0];
             }
             try stdout.print("\n", .{});
         }
