b48852dea2f140a56293487e1c6df74879b8dec8
Alexander Rummel <alexander.rummel@man.eu>
2021-04-18 13:58:35 +0200

    perm: transpositions and multiplication

--- perm/src/main.zig
+++ perm/src/main.zig
@@ -13,6 +13,19 @@ pub const Perm = struct {
         }
     }
 
+    pub fn swap_elements(self: *Perm, i: usize, j: usize) void {
+        const e = self.elements[i];
+        self.elements[i] = self.elements[j];
+        self.elements[j] = e;
+    }
+
+    pub fn multiply(self: *Perm, a: *const Perm, b: *const Perm) void {
+        var i: usize = 0;
+        while (i < len) : (i += 1) {
+            self.elements[i] = b.elements[a.elements[i]];
+        }
+    }
+
     pub fn print(self: *const Perm, out: std.fs.File) !void {
         const writer = out.writer();
         try writer.writeAll("perm[");
@@ -30,4 +43,12 @@ pub fn main() !void {
     var a: Perm = undefined;
     a.identity();
     try a.print(stdout);
+    var b = a;
+    b.swap_elements(0, 1);
+    b.swap_elements(1, 2);
+    b.swap_elements(2, 3);
+    try b.print(stdout);
+    var c: Perm = undefined;
+    c.multiply(&b, &b);
+    try c.print(stdout);
 }
