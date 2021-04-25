7ba74e06eb13e6eb2e3849bc8a29d3b12cae7fd0
Alexander Rummel <alexander.rummel@man.eu>
2021-04-25 16:16:20 +0200

    interactive roman numerals converter

--- roman_num/src/main.zig
+++ roman_num/src/main.zig
@@ -5,8 +5,6 @@ const roman_num = @import("roman_num.zig");
 const expect = @import("std").testing.expect;
 const expectError = @import("std").testing.expectError;
 
-const stdout = std.io.getStdOut();
-
 test "single roman uppercase letters" {
     expect(1 == try roman_num.parse("I"));
     expect(5 == try roman_num.parse("V"));
@@ -76,8 +74,25 @@ test "invalid roman letters" {
     expectError(roman_num.ParseError.InvalidLetter, roman_num.parse("A"));
 }
 
-pub fn main() anyerror!void {
-    const roman = "MCMIX";
-    var value = try roman_num.parse(roman);
-    try stdout.writer().print("{s} = {d}\n", .{ roman, value });
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn();
+    while (true) {
+        try stdout.print("Enter a roman numeral: ", .{});
+        var line_buf: [20]u8 = undefined;
+        const amt = try stdin.read(&line_buf);
+        if (amt == line_buf.len) {
+            try stdout.print("Input too long.\n", .{});
+            break;
+        }
+        const line = std.mem.trimRight(u8, line_buf[0..amt], "\r\n");
+        const value = roman_num.parse(line) catch |err| {
+            try stdout.print("The input '{s}' is not a roman numeral.\n", .{line});
+            continue;
+        };
+        if (value == 0) {
+            break;
+        }
+        try stdout.print("The roman numeral {s} is {d}\n", .{ line, value });
+    }
 }
