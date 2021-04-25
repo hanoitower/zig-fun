17ca6e0d7d06fa7a20863e19e5a2a64860b6784d
Alexander Rummel <alexander.rummel@man.eu>
2021-04-25 11:37:32 +0200

    roman numbers: first simple implementation

--- roman_num/src/main.zig
+++ roman_num/src/main.zig
@@ -2,7 +2,19 @@ const std = @import("std");
 
 const roman_num = @import("roman_num.zig");
 
+const expect = @import("std").testing.expect;
+
 const stdout = std.io.getStdOut();
+
+test "single roman uppercase letters" {
+    expect(roman_num.parse("I") == 1);
+    expect(roman_num.parse("V") == 5);
+    expect(roman_num.parse("X") == 10);
+    expect(roman_num.parse("L") == 50);
+    expect(roman_num.parse("C") == 100);
+    expect(roman_num.parse("D") == 500);
+    expect(roman_num.parse("M") == 1000);
+}
 
 pub fn main() anyerror!void {
     const roman = "MCMIX";
--- roman_num/src/roman_num.zig
+++ roman_num/src/roman_num.zig
@@ -5,5 +5,25 @@
 //
 
 pub fn parse(roman_num: []const u8) u16 {
-    return 0;
+    var value: u16 = 0;
+    for (roman_num) |roman_letter| {
+        if (roman_letter == 'I') {
+            value += 1;
+        } else if (roman_letter == 'V') {
+            value += 5;
+        } else if (roman_letter == 'X') {
+            value += 10;
+        } else if (roman_letter == 'L') {
+            value += 50;
+        } else if (roman_letter == 'C') {
+            value += 100;
+        } else if (roman_letter == 'D') {
+            value += 500;
+        } else if (roman_letter == 'M') {
+            value += 1000;
+        } else {
+            // ignore all other letters
+        }
+    }
+    return value;
 }
