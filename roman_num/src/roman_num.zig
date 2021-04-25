7642e9475b2a44e2bec949dbfad91f57010a6eda
Alexander Rummel <alexander.rummel@man.eu>
2021-04-25 13:20:46 +0200

    use switch in parseLetter

--- roman_num/src/roman_num.zig
+++ roman_num/src/roman_num.zig
@@ -7,23 +7,16 @@
 pub const ParseError = error{InvalidLetter};
 
 fn parseLetter(roman_letter: u8) ParseError!u16 {
-    if (roman_letter == 'I' or roman_letter == 'i') {
-        return 1;
-    } else if (roman_letter == 'V' or roman_letter == 'v') {
-        return 5;
-    } else if (roman_letter == 'X' or roman_letter == 'x') {
-        return 10;
-    } else if (roman_letter == 'L' or roman_letter == 'l') {
-        return 50;
-    } else if (roman_letter == 'C' or roman_letter == 'c') {
-        return 100;
-    } else if (roman_letter == 'D' or roman_letter == 'd') {
-        return 500;
-    } else if (roman_letter == 'M' or roman_letter == 'm') {
-        return 1000;
-    } else {
-        return error.InvalidLetter;
-    }
+    return switch (roman_letter) {
+        'I', 'i' => 1,
+        'V', 'v' => 5,
+        'X', 'x' => 10,
+        'L', 'l' => 50,
+        'C', 'c' => 100,
+        'D', 'd' => 500,
+        'M', 'm' => 1000,
+        else => error.InvalidLetter,
+    };
 }
 
 pub fn parse(roman_num: []const u8) ParseError!u16 {
