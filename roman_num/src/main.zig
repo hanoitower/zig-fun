c19d639a450f6afaa4722ed77c042e26ddff2e93
Alexander Rummel <alexander.rummel@man.eu>
2021-04-25 11:16:39 +0200

    roman_num package started

--- roman_num/src/main.zig
+++ roman_num/src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
+const roman_num = @import("roman_num.zig");
+
+const stdout = std.io.getStdOut();
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const roman = "MCMIX";
+    var value = roman_num.parse(roman);
+    try stdout.writer().print("{s} = {d}\n", .{ roman, value });
 }
---
+++ roman_num/src/roman_num.zig
@@ -0,0 +1,9 @@
+//
+// roman_num.zig
+//
+// roman numerals
+//
+
+pub fn parse(roman_num: []const u8) u16 {
+    return 0;
+}
