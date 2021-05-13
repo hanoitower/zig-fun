ab0a937510f664c4ebc549f30fc515283d1cc71e
Alexander Rummel <alexander.rummel@man.eu>
2021-05-13 13:31:15 +0200

    write file content to stdout

--- readlines/src/main.zig
+++ readlines/src/main.zig
@@ -7,13 +7,15 @@ const std = @import("std");
 const File = std.fs.File;
 
 pub fn main() !void {
-    var my_file = try std.fs.cwd().openFile("build.zig", .{});
+    const my_file = try std.fs.cwd().openFile("build.zig", .{});
     defer my_file.close();
+    const std_out = std.io.getStdOut();
     var buffer: [8]u8 = undefined;
     while (true) {
         const num_read = try my_file.read(&buffer);
         if (num_read == 0) break;
-        std.log.info("read {} bytes.", .{num_read});
+        // std.log.info("read {} bytes.", .{num_read});
+        try std_out.writeAll(buffer[0..num_read]);
     }
-    std.log.info("done.", .{});
+    // std.log.info("done.", .{});
 }
