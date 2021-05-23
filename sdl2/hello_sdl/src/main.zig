56d1d8936c7915e2044380c499d09b39378bf7eb
Alexander Rummel <alexander.rummel@man.eu>
2021-05-23 12:04:46 +0200

    add sdl2 to build system and initialize it

--- sdl2/hello_sdl/src/main.zig
+++ sdl2/hello_sdl/src/main.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+const c = @cImport({
+    @cInclude("SDL.h");
+});
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    std.log.info("Initializing SDL...", .{});
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        std.log.emerg("Unable to initialize SDL: {s}", .{c.SDL_GetError()});
+        return error.Failed;
+    }
+    defer c.SDL_Quit();
+    std.log.info("Initializing SDL done.", .{});
 }
