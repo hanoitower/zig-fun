1f7294e895500069eae592089533456a1fa2eb4f
Alexander Rummel <alexander.rummel@man.eu>
2021-05-23 12:21:32 +0200

    sdl: show empty window for 3 seconds

--- sdl2/hello_sdl/src/main.zig
+++ sdl2/hello_sdl/src/main.zig
@@ -12,4 +12,23 @@ pub fn main() anyerror!void {
     }
     defer c.SDL_Quit();
     std.log.info("Initializing SDL done.", .{});
+
+    const window = c.SDL_CreateWindow("Hello SDL2", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, 640, 480, 0) orelse {
+        std.log.emerg("Unable to create window: {s}", .{c.SDL_GetError()});
+        return error.Failed;
+    };
+    defer c.SDL_DestroyWindow(window);
+
+    const renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_PRESENTVSYNC) orelse {
+        std.log.emerg("Unable to create SDL renderer: {s}", .{c.SDL_GetError()});
+        return error.Failed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, c.SDL_ALPHA_OPAQUE);
+    _ = c.SDL_RenderClear(renderer);
+
+    c.SDL_RenderPresent(renderer);
+
+    c.SDL_Delay(3000);
 }
