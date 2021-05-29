ce7bb4ddb3ed8563622f660159495931d1d4b92d
Alexander Rummel <alexander.rummel@man.eu>
2021-05-29 18:56:05 +0200

    sdl: drawMap function started

--- sdl2/hello_sdl/src/main.zig
+++ sdl2/hello_sdl/src/main.zig
@@ -7,19 +7,31 @@
 const c = @import("c.zig");
 const simple_sdl = @import("simple_sdl.zig");
 
+const dx = 32;
+const dy = 32;
+const nx = 30;
+const ny = 20;
+
+const width = (nx + 1) * dx;
+const height = (ny + 1) * dy;
+
+pub fn drawMap(renderer: *simple_sdl.Renderer) !void {
+    try renderer.setDrawColor(0, 0, 0, c.SDL_ALPHA_OPAQUE);
+    try renderer.clear();
+    try renderer.setDrawColor(0xff, 0x80, 0, c.SDL_ALPHA_OPAQUE);
+    try renderer.fillRect(10, 10, 100, 100);
+}
+
 pub fn main() !void {
     var sdl = try simple_sdl.init();
     defer sdl.deinit();
-    var window = try sdl.createWindow();
+    var window = try sdl.createWindow(width, height);
     defer window.deinit();
     var renderer = try window.createRenderer();
     defer renderer.deinit();
     while (!sdl.quit) {
         sdl.tick();
-        try renderer.setDrawColor(0, 0, 0, c.SDL_ALPHA_OPAQUE);
-        try renderer.clear();
-        try renderer.setDrawColor(0xff, 0x80, 0, c.SDL_ALPHA_OPAQUE);
-        try renderer.fillRect(10, 10, 100, 100);
+        try drawMap(&renderer);
         renderer.present();
     }
 }
