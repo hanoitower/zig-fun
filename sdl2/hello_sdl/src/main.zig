b44f29b9e8f71c6a61ccd1ec44c33ffd5c07a844
Alexander Rummel <alexander.rummel@man.eu>
2021-05-29 19:09:10 +0200

    sdl: random flicker map

--- sdl2/hello_sdl/src/main.zig
+++ sdl2/hello_sdl/src/main.zig
@@ -6,6 +6,7 @@
 
 const c = @import("c.zig");
 const simple_sdl = @import("simple_sdl.zig");
+const myrand = @import("myrand.zig");
 
 const Map = struct {
     const dx = 32;
@@ -18,7 +19,7 @@ const Map = struct {
     const width = (nx + 1) * dx;
     const height = (ny + 1) * dy;
 
-    pub fn draw(renderer: *simple_sdl.Renderer) !void {
+    pub fn draw(renderer: *simple_sdl.Renderer, prng: *myrand.Prng) !void {
         try renderer.setDrawColor(0, 0, 0, c.SDL_ALPHA_OPAQUE);
         try renderer.clear();
 
@@ -28,14 +29,18 @@ const Map = struct {
             var ix: i32 = 0;
             while (ix < nx) : (ix += 1) {
                 const x = x0 + ix * dx;
-                try renderer.setDrawColor(0xff, 0x80, 0, c.SDL_ALPHA_OPAQUE);
-                try renderer.fillRect(x, y, dx / 2, dy / 2);
+                const red = prng.uintLessThan(u8, 0xff);
+                const green = prng.uintLessThan(u8, 0xff);
+                const blue = prng.uintLessThan(u8, 0xff);
+                try renderer.setDrawColor(red, green, blue, c.SDL_ALPHA_OPAQUE);
+                try renderer.fillRect(x, y, dx, dy);
             }
         }
     }
 };
 
 pub fn main() !void {
+    var prng = try myrand.Prng.create();
     var sdl = try simple_sdl.init();
     defer sdl.deinit();
     var window = try sdl.createWindow(Map.width, Map.height);
@@ -44,7 +49,7 @@ pub fn main() !void {
     defer renderer.deinit();
     while (!sdl.quit) {
         sdl.tick();
-        try Map.draw(&renderer);
+        try Map.draw(&renderer, &prng);
         renderer.present();
     }
 }
