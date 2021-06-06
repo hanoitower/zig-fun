a9f49d054c023bf4b2df80e755a6c4d078828fdb
Alexander Rummel <alexander.rummel@man.eu>
2021-06-06 15:41:16 +0200

    myrand: adapt to new zig version 0.8.0

--- random/src/main.zig
+++ random/src/main.zig
@@ -5,7 +5,7 @@ const myrand = @import("myrand.zig");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Guessing Game\n", .{});
-    var prng = try myrand.Prng.create();
+    var prng = myrand.Prng.create();
     while (true) {
         const answer = prng.uintLessThan(u8, 100) + 1;
         try stdout.print("random number: {d}\n", .{answer});
